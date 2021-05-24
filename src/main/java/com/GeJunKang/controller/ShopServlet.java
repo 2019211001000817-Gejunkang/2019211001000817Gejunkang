af58d10519e4c7dccbc6e779b2edc96206a7e38d
2019211001000817-Gejunkang <67791754+2019211001000817-Gejunkang@users.noreply.github.com>
2021-05-24 20:33:08 +0800

    Add files via upload

--- src/main/java/com/GeJunKang/controller/ShopServlet.java
+++ src/main/java/com/GeJunKang/controller/ShopServlet.java
@@ -1,6 +1,7 @@
 package com.GeJunKang.controller;
 
 import com.GeJunKang.dao.ProductDao;
+import com.GeJunKang.model.Category;
 import com.GeJunKang.model.Product;
 
 import javax.servlet.*;
@@ -11,8 +12,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet(name = "ProductListServlet", value = "/admin/productList")
-public class ProductListServlet extends HttpServlet {
+@WebServlet(name = "ShopServlet", value = "/shop")
+public class ShopServlet extends HttpServlet {
     Connection con=null;
 
     @Override
@@ -20,17 +21,29 @@ public class ProductListServlet extends HttpServlet {
         super.init();;
         con=(Connection) getServletContext().getAttribute("con");
     }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDao=new ProductDao();
+        Category category=new Category();
         try {
-            List<Product> productList=productDao.findAll(con);
-            request.setAttribute("productList",productList);
+            List<Category> categoryList=category.findallCategory(con);
+            request.setAttribute("categoryList",categoryList);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        String path="/WEB-INF/views/admin/productList.jsp";
+        ProductDao productDao=new ProductDao();
+        try {
+            if(request.getParameter("categoryId")==null) {
+                List<Product> productList = productDao.findAll(con);
+                request.setAttribute("productList", productList);
+            }else{
+                int catId=Integer.parseInt(request.getParameter("categoryId"));
+                List<Product> productList =productDao.findByCategoryId(catId,con);
+                request.setAttribute("productList", productList);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        String path="/WEB-INF/views/shop.jsp";
         request.getRequestDispatcher(path).forward(request,response);
     }
 
